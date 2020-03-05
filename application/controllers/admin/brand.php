c63890d4629f0f20a42ad65ec78c3018490170fc
cowen2002 <cowen2002@qq.com>
2020-03-05 17:48:02 +0800

    add uploads logo

--- application/controllers/admin/brand.php
+++ application/controllers/admin/brand.php
@@ -33,33 +33,52 @@ class Brand extends Admin_Controller{
 			$data['url'] = base_url('admin/brand/add');
 			$data['wait'] = 5;
 			$this->load->view('message.html', $data);
-		}
-		else{
-			#通过验证
-			$arr = array(
-				'brand_name' => $this->input->post('brand_name',true),
-				'brand_desc' => $this->input->post('brand_desc',true),
-				'url' => $this->input->post('url',true),
-				'sort_order' => $this->input->post('sort_order',true),
-				'is_show' => $this->input->post('is_show')
-			);
-			if($this->brand_model->brand_add($arr)){
-				$data = array(
-					'url' => base_url('admin/brand/index'),
-					'message' => '添加品牌成功',
-					'wait' => 2
-				);
-			$this->load->view('message.html', $data);
-			}else{
-				$data = array(
-					'url' => base_url('admin/brand/add'),
-					'message' => '添加品牌失败',
-					'wait' => 3
-				);
-				$this->load->view('message.html', $data);
-			}
-			
-		}
+		}else{
+				#通过验证
 
+				#添加品牌logo，文件上传类处理方法
+				$config['upload_path'] = './public/uploads/';
+				$config['allowed_types'] = 'gif|jpg|png';
+				$config['max_size'] = '0';
+				// $config['max_width'] = '1024';
+				// $config['max_height'] = '768';
+				$this->load->library('upload', $config);
+				if($this->upload->do_upload('logo')){
+					#上传成功
+					$fileInfo = $this->upload->data();
+
+					$arr = array(
+						'brand_name' => $this->input->post('brand_name',true),
+						'brand_desc' => $this->input->post('brand_desc',true),
+						'url' => $this->input->post('url',true),
+						'sort_order' => $this->input->post('sort_order',true),
+						'is_show' => $this->input->post('is_show'),
+						'logo' => $fileInfo['file_name']
+					);
+					if($this->brand_model->brand_add($arr)){
+					$data = array(
+						'url' => base_url('admin/brand/index'),
+						'message' => '添加品牌成功',
+						'wait' => 2
+					);
+					$this->load->view('message.html', $data);
+					}else{
+						$data = array(
+							'url' => base_url('admin/brand/add'),
+							'message' => '添加品牌失败',
+							'wait' => 3
+						);
+						$this->load->view('message.html', $data);
+					}
+				}else{
+					#上传失败
+						$data = array(
+							'url' => base_url('admin/brand/add'),
+							'message' => $this->upload->display_errors(),
+							'wait' => 3
+						);
+						$this->load->view('message.html', $data);
+					}	
+			}
 	}
 }
