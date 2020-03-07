6795774a2fb810708457c538f2d9a83c7629dfe7
cowen2002 <cowen2002@qq.com>
2020-03-08 01:12:57 +0800

    goods type delete ,edit have been finished

--- application/controllers/admin/goods_type.php
+++ application/controllers/admin/goods_type.php
@@ -20,8 +20,9 @@ class Goods_type extends Admin_Controller{
 		$this->load->view('goods_type_add.html');
 	}
 
-	public function edit(){
-		$this->load->view('goods_type_edit.html');
+	public function edit($goods_type_id){
+		$data['goods_type_info'] = $this->goods_type_model->get_goods_type_by_id($goods_type_id);
+		$this->load->view('goods_type_edit.html', $data);
 	}
 
 	public function goods_type_add(){
@@ -55,9 +56,43 @@ class Goods_type extends Admin_Controller{
 		}
 	}
 
+	public function goods_type_update(){
+			$this->form_validation->set_rules('goods_type_name', '商品类型名称','trim|required');
+			if($this->form_validation->run()==false){
+				#未通过验证
+				$data['message'] = validation_errors();
+				$data['url'] = base_url('admin/goods_type/index');
+				$data['wait'] = 5;
+				$this->load->view('message.html', $data);
+			}else{
+					#通过验证
+					$arr = array(
+								'goods_type_id' => $this->input->post('goods_type_id',true),
+								'goods_type_name' => $this->input->post('goods_type_name',true)
+							);
+					if($this->goods_type_model->goods_type_update($arr)){
+					$data = array(
+						'url' => base_url('admin/goods_type/index'),
+						'message' => '修改商品类型成功',
+						'wait' => 2
+					);
+					$this->load->view('message.html', $data);
+				}else{
+					$data = array(
+						'url' => base_url('admin/goods_type/index'),
+						'message' => '修改商品类型失败',
+						'wait' => 3
+					);
+					$this->load->view('message.html', $data);
+				}
 
+			}
+		}
 
-
+	public function goods_type_delete($id){
+		$this->goods_type_model->delete_goods_type_by_id($id);
+		$this->index();
+	}
 
 
 
--- application/models/goods_type_model.php
+++ application/models/goods_type_model.php
@@ -10,4 +10,23 @@ class Goods_type_model extends CI_Model{
 		$query = $this->db->get('ci_goods_type');
 		return $query->result_array();
 	}
+
+	public function get_goods_type_by_id($id){
+		$sql = 'select * from ci_goods_type where goods_type_id='.$id;
+		$query = $this->db->query($sql);
+		return $query->row_array();
+	}
+
+	public function goods_type_update($arr){
+		$this->db->where('goods_type_id', $arr['goods_type_id']);
+		return $this->db->update('ci_goods_type', $arr);
+	}
+
+	public function delete_goods_type_by_id($id){
+		$this->db->delete('ci_goods_type', array('goods_type_id'=>$id));
+	}
+
+
+
+
 }
